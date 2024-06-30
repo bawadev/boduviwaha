687ad0ad081822823bb97789634aace53369c2f0
PasinduBm <pasindumunasinghe94@gmail.com>
2024-07-01 00:46:03 +0530

    fix build errors

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import Router from 'next/router';
 import { ThemeUIProvider } from 'theme-ui'
 import theme from '../theme';
-import Layout from '../components/Layout';
+import Layout from '../components/layout';
 import 'swiper/css';
 import '../../public/assets/css/slick.min.css';
 import { initGA, logPageView } from '../analytics';
