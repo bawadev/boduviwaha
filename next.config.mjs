90bf70945166d32ec31bf99928c912f13e1ec23f
PasinduBm <pasindumunasinghe94@gmail.com>
2024-07-19 10:18:21 +0530

    changeUrl backend

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: '',
+    basePath: '/boduviwaha',
 };
 
 export default nextConfig;
--- src/components/forms/registration/registration-form.js
+++ src/components/forms/registration/registration-form.js
@@ -13,7 +13,7 @@ const steps = [
   { component: SubmissionSuccess, title: "ස්තුතියි" },
 ];
 
-const backendBaseUrl = "https://ec2-16-171-153-216.eu-north-1.compute.amazonaws.com"
+ const backEndBaseUrl = "https://ec2-16-171-153-216.eu-north-1.compute.amazonaws.com"
 // const baseUrl = "http://localhost:8080"
 
 export default function RegisterMultiStepForm() {
@@ -32,178 +32,219 @@ export default function RegisterMultiStepForm() {
   const handleNextStep = async (data) => {
     console.log("form Data==============");
     console.log(data);
-    try {
-      switch (currentStep) {
-        case 0:
-          await submitPersonalInfo(data);
-          break;
-        case 1:
-          await submitSpiritualInfo(data);
-          break;
-        case 2:
-          await submitAdditionalInfo(data);
-          break;
-        default:
-          break;
-      }
-      setFormData({ ...formData, ...data });
-      setCurrentStep(currentStep + 1);
-    } catch (error) {
-      console.error("Error submitting form:", error);
-      // Handle error (e.g., show an error message)
+    switch (currentStep) {
+      case 0:
+        try {
+          console.log("Submiting first form data..");
+
+          // First API call to create a Buddhist user
+          const userResponse = await fetch(
+            backEndBaseUrl+"/api/buddhist-users",
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                // Assuming the backend will assign the actual ID
+                firstName: data.firstName,
+                lastName: data.lastName,
+                dateOfBirth: new Date(data.dateOfBirth)
+                  .toISOString()
+                  .split("T")[0], // Format as YYYY-MM-DD
+                gender: data.gender, // You might want to add a gender field to your form if it's not always "MALE"
+              }),
+            }
+          );
+
+          if (!userResponse.ok) {
+            throw new Error("Failed to submit user data");
+          }
+
+          const updatedData = await userResponse.json();
+          setUserData(updatedData);
+          console.log("User data submitted successfully:", userData);
+
+          // Second API call to create an address
+          const addressResponse = await fetch(
+            backEndBaseUrl+"/api/addresses",
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                address: data.address,
+                province: data.province,
+                district: data.district,
+                nearestTown: data.nearestTown,
+                user: {
+                  userId: updatedData.userId,
+                },
+              }),
+            }
+          );
+
+          console.log("address response =======");
+          console.log(addressResponse);
+
+          if (!addressResponse.ok) {
+            throw new Error("Failed to submit address data");
+          }
+
+          const addressData = await addressResponse.json();
+          console.log("Address data submitted successfully:", addressData);
+        } catch (error) {
+          console.error("Error submitting form:", error);
+          // Handle error (e.g., show an error message)
+        }
+
+        break;
+
+      case 1:
+        try {
+          console.log("Submiting first form data..");
+
+          // First API call to create a Buddhist user
+          // Second API call to create Buddhist practice information
+
+          const practiceResponse = await fetch(
+            backEndBaseUrl+"/api/buddhist-practices",
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                origin: data.origin,
+                timeInvestedOverall: data.timeInvestedOvarall,
+                deedCategoryDana: data.deedCategoryDana,
+                deedCategorySeela: data.deedCategorySeela,
+                deedCategoryBhavana: data.deedCategoryBhavana,
+                deedCategoryOther: data.deedCategoryOther,
+                meditationAnaPanaSathiTime: parseInt(
+                  data.meditationAnaPanaSathiTime
+                ),
+                meditationMayithreeTime: parseInt(data.meditationMayithreeTime),
+                bodyAwarenessTime: parseInt(data.bodyAwarenessTime),
+                meditationOtherTime: parseInt(data.meditationOtherTime),
+                meditationTeacher: data.meditationTeacher,
+                seelaPansilTime: parseInt(data.seelaPansilTime),
+                seelaAtaSilTime: parseInt(data.seelaAtaSilTime),
+                seelaOtherTime: parseInt(data.seelaOtherTime),
+                danaAmountAnimals: parseInt(data.danaAmountAnimals),
+                danaAmountPeople: parseInt(data.danaAmountPeople),
+                danaAmountSangha: parseInt(data.danaAmountSangha),
+                sermonListenTime: parseInt(data.sermonListenTime),
+                sermonSpeakersDetails: data.sermonSpeakersDetails,
+                knowledgeAbhiDhamma: data.knowledgeAbhiDhamma,
+                descriptionOfYourSelf: data.descriptionOfYourSelf,
+                enterToHardPractice: data.enterToHardPractice,
+                user: {
+                  userId: userData.userId,
+                },
+              }),
+            }
+          );
+
+          if (!practiceResponse.ok) {
+            throw new Error("Failed to submit Buddhist practice data");
+          }
+
+          const practiceData = await practiceResponse.json();
+          console.log(
+            "Buddhist practice data submitted successfully:",
+            practiceData
+          );
+        } catch (error) {
+          console.error("Error submitting form:", error);
+          // Handle error (e.g., show an error message)
+        }
+        break;
+
+      case 2:
+        try {
+          console.log("Submiting social info data..");
+
+          // Second API call to create an address
+          const addressResponse = await fetch(
+            backEndBaseUrl+"/api/social-information",
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                marriageStatus: data.marriageStatus,
+                occupation: data.occupation,
+                highestEducationQualification: data.highestEducationQualification,
+                jobOrProfession: data.jobOrProfession,
+                monthlyIncome: data.monthlyIncome,
+                houseOwnership: data.houseOwnership,
+                vehicleOwnership: data.vehicleOwnership,
+                createdAt: new Date().toISOString(),
+                user: { userId: userData.userId }  // Assuming you have userId in your form data
+              }),
+            }
+          );
+
+          console.log("social info response =======");
+          console.log(addressResponse);
+
+          if (!addressResponse.ok) {
+            throw new Error("Failed to submit social info data");
+          }
+
+          const addressData = await addressResponse.json();
+          console.log("social info data submitted successfully:", addressData);
+
+          // First API call to create a Buddhist user
+          const userResponse = await fetch(
+            backEndBaseUrl+"/api/health-information",
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                height: data.height,
+                weight: data.weight,
+                physicalAttractiveness: data.physicalAttractiveness,
+                skinTone: data.skinTone,
+                kidsExpectancy: data.kidsExpectancy,
+                smoking: data.smoking,
+                drugUsage: data.drugUsage,
+                healthCondition: data.healthCondition,
+                disability: data.disability,
+                mentalHealth: data.mentalHealth,
+                geneticRisks: data.geneticRisks,
+                yourMessage: data.yourMessage,
+                createdAt: new Date().toISOString(),
+                user: { userId: userData.userId }, // Assuming you have userId in your form data
+              }),
+            }
+          );
+
+          if (!userResponse.ok) {
+            throw new Error("Failed to submit helth info data");
+          }
+
+          const updatedData = await userResponse.json();
+          setUserData(updatedData);
+          console.log("helth info data submitted successfully:", userData);
+
+          
+        } catch (error) {
+          console.error("Error submitting form:", error);
+          // Handle error (e.g., show an error message)
+        }
+        break;
+
+      default:
+        break;
     }
-  };
-
-  const submitPersonalInfo = async (data) => {
-    console.log("Submitting personal info...");
-    // First API call to create a Buddhist user
-    const userResponse = await fetch(`${backendBaseUrl}/api/buddhist-users`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        firstName: data.firstName,
-        lastName: data.lastName,
-        dateOfBirth: new Date(data.dateOfBirth).toISOString().split("T")[0],
-        gender: data.gender,
-      }),
-    });
-
-    if (!userResponse.ok) {
-      throw new Error("Failed to submit user data");
-    }
-
-    const updatedData = await userResponse.json();
-    setUserData(updatedData);
-    console.log("User data submitted successfully:", updatedData);
-
-    // Second API call to create an address
-    const addressResponse = await fetch(`${backendBaseUrl}/api/addresses`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        address: data.address,
-        province: data.province,
-        district: data.district,
-        nearestTown: data.nearestTown,
-        user: {
-          userId: updatedData.userId,
-        },
-      }),
-    });
-
-    if (!addressResponse.ok) {
-      throw new Error("Failed to submit address data");
-    }
-
-    const addressData = await addressResponse.json();
-    console.log("Address data submitted successfully:", addressData);
-  };
-
-  const submitSpiritualInfo = async (data) => {
-    console.log("Submitting spiritual info...");
-    const practiceResponse = await fetch(`${backendBaseUrl}/api/buddhist-practices`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        origin: data.origin,
-        timeInvestedOverall: data.timeInvestedOverall,
-        deedCategoryDana: data.deedCategoryDana,
-        deedCategorySeela: data.deedCategorySeela,
-        deedCategoryBhavana: data.deedCategoryBhavana,
-        deedCategoryOther: data.deedCategoryOther,
-        meditationAnaPanaSathiTime: parseInt(data.meditationAnaPanaSathiTime),
-        meditationMayithreeTime: parseInt(data.meditationMayithreeTime),
-        bodyAwarenessTime: parseInt(data.bodyAwarenessTime),
-        meditationOtherTime: parseInt(data.meditationOtherTime),
-        meditationTeacher: data.meditationTeacher,
-        seelaPansilTime: parseInt(data.seelaPansilTime),
-        seelaAtaSilTime: parseInt(data.seelaAtaSilTime),
-        seelaOtherTime: parseInt(data.seelaOtherTime),
-        danaAmountAnimals: parseInt(data.danaAmountAnimals),
-        danaAmountPeople: parseInt(data.danaAmountPeople),
-        danaAmountSangha: parseInt(data.danaAmountSangha),
-        sermonListenTime: parseInt(data.sermonListenTime),
-        sermonSpeakersDetails: data.sermonSpeakersDetails,
-        knowledgeAbhiDhamma: data.knowledgeAbhiDhamma,
-        descriptionOfYourSelf: data.descriptionOfYourSelf,
-        enterToHardPractice: data.enterToHardPractice,
-        user: {
-          userId: userData.userId,
-        },
-      }),
-    });
-
-    if (!practiceResponse.ok) {
-      throw new Error("Failed to submit Buddhist practice data");
-    }
-
-    const practiceData = await practiceResponse.json();
-    console.log("Buddhist practice data submitted successfully:", practiceData);
-  };
-
-  const submitAdditionalInfo = async (data) => {
-    console.log("Submitting additional info...");
-    const socialInfoResponse = await fetch(`${backendBaseUrl}/api/social-information`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        marriageStatus: data.marriageStatus,
-        occupation: data.occupation,
-        highestEducationQualification: data.highestEducationQualification,
-        jobOrProfession: data.jobOrProfession,
-        monthlyIncome: data.monthlyIncome,
-        houseOwnership: data.houseOwnership,
-        vehicleOwnership: data.vehicleOwnership,
-        createdAt: new Date().toISOString(),
-        user: { userId: userData.userId },
-      }),
-    });
-
-    if (!socialInfoResponse.ok) {
-      throw new Error("Failed to submit social info data");
-    }
-
-    const socialInfoData = await socialInfoResponse.json();
-    console.log("Social info data submitted successfully:", socialInfoData);
-
-    const healthInfoResponse = await fetch(`${backendBaseUrl}/api/health-information`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        height: data.height,
-        weight: data.weight,
-        physicalAttractiveness: data.physicalAttractiveness,
-        skinTone: data.skinTone,
-        kidsExpectancy: data.kidsExpectancy,
-        smoking: data.smoking,
-        drugUsage: data.drugUsage,
-        healthCondition: data.healthCondition,
-        disability: data.disability,
-        mentalHealth: data.mentalHealth,
-        geneticRisks: data.geneticRisks,
-        yourMessage: data.yourMessage,
-        createdAt: new Date().toISOString(),
-        user: { userId: userData.userId },
-      }),
-    });
-
-    if (!healthInfoResponse.ok) {
-      throw new Error("Failed to submit health info data");
-    }
-
-    const healthInfoData = await healthInfoResponse.json();
-    setUserData(healthInfoData);
-    console.log("Health info data submitted successfully:", healthInfoData);
+    setFormData({ ...formData, ...data });
+    setCurrentStep(currentStep + 1);
   };
 
   const handlePreviousStep = () => {
