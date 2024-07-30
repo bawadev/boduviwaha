7c4f252a50b50e036f229bc5d182beb55fcde9f3
PasinduBm <pasindumunasinghe94@gmail.com>
2024-07-30 10:50:34 +0530

    changed image uris

--- src/resource/images.js
+++ src/resource/images.js
@@ -1,100 +1,100 @@
 const siteImages = {
   logo: {
-    src: '/boduviwaha/assets/images/logo-white-lk.png',
+    src: 'boduviwaha.lk/assets/images/logo-white-lk.png',
   },
   bannerBg: {
-    src: '/boduviwaha/assets/images/banner-bg.jpg',
+    src: 'boduviwaha.lk/assets/images/banner-bg.jpg',
   },
   chat: {
-    src: '/boduviwaha/assets/images/icons/chat.png',
+    src: 'boduviwaha.lk/assets/images/icons/chat.png',
   },
   call: {
-    src: '/boduviwaha/assets/images/icons/call.png',
+    src: 'boduviwaha.lk/assets/images/icons/call.png',
   },
   play: {
-    src: '/boduviwaha/assets/images/icons/play.png',
+    src: 'boduviwaha.lk/assets/images/icons/play.png',
   },
   pause: {
-    src: '/boduviwaha/assets/images/icons/pause.png',
+    src: 'boduviwaha.lk/assets/images/icons/pause.png',
   },
   close: {
-    src: '/boduviwaha/assets/images/icons/close.png',
+    src: 'boduviwaha.lk/assets/images/icons/close.png',
   },
   'menu-icon': {
-    src: '/boduviwaha/assets/images/icons/menu-icon.png',
+    src: 'boduviwaha.lk/assets/images/icons/menu-icon.png',
   },
   'user-icon': {
-    src: '/boduviwaha/assets/images/icons/user-icon.png',
+    src: 'boduviwaha.lk/assets/images/icons/user-icon.png',
   },
   'arrow-down': {
-    src: '/boduviwaha/assets/images/icons/arrow-down.png',
+    src: 'boduviwaha.lk/assets/images/icons/arrow-down.png',
   },
  
   thumb: [
-    { src: '/boduviwaha/assets/images/blog/1.png' },
-    { src: '/boduviwaha/assets/images/blog/2.png' },
-    { src: '/boduviwaha/assets/images/blog/3.png' }
+    { src: 'boduviwaha.lk/assets/images/blog/1.png' },
+    { src: 'boduviwaha.lk/assets/images/blog/2.png' },
+    { src: 'boduviwaha.lk/assets/images/blog/3.png' }
   ],
   // Additional images with replaced paths
   userIcon: {
-    src: '/boduviwaha/assets/images/icons/two-users.png',
+    src: 'boduviwaha.lk/assets/images/icons/two-users.png',
   },
   userIcon2: {
-    src: '/boduviwaha/assets/images/icons/three-users.png',
+    src: 'boduviwaha.lk/assets/images/icons/three-users.png',
   },
   customer: {
-    src: '/boduviwaha/assets/images/customer.png',
+    src: 'boduviwaha.lk/assets/images/customer.png',
   },
   gallery1: {
-    src: '/boduviwaha/assets/images/gallery/1.png',
+    src: 'boduviwaha.lk/assets/images/gallery/1.png',
   },
   gallery2: {
-    src: '/boduviwaha/assets/images/gallery/2.png',
+    src: 'boduviwaha.lk/assets/images/gallery/2.png',
   },
   gallery3: {
-    src: '/boduviwaha/assets/images/gallery/3.png',
+    src: 'boduviwaha.lk/assets/images/gallery/3.png',
   },
   gallery4: {
-    src: '/boduviwaha/assets/images/gallery/4.png',
+    src: 'boduviwaha.lk/assets/images/gallery/4.png',
   },
   gallery5: {
-    src: '/boduviwaha/assets/images/gallery/5.png',
+    src: 'boduviwaha.lk/assets/images/gallery/5.png',
   },
   gallery6: {
-    src: '/boduviwaha/assets/images/gallery/6.png',
+    src: 'boduviwaha.lk/assets/images/gallery/6.png',
   },
   expand: {
-    src: '/boduviwaha/assets/images/icons/expand.png',
+    src: 'boduviwaha.lk/assets/images/icons/expand.png',
   },
   users: {
-    src: '/boduviwaha/assets/images/icons/users.png',
+    src: 'boduviwaha.lk/assets/images/icons/users.png',
   },
   wifi: {
-    src: '/boduviwaha/assets/images/icons/wifi.png',
+    src: 'boduviwaha.lk/assets/images/icons/wifi.png',
   },
   slider1: {
-    src: '/boduviwaha/assets/images/features/1.png',
+    src: 'boduviwaha.lk/assets/images/features/1.png',
   },
   slider2: {
-    src: '/boduviwaha/assets/images/features/2.png',
+    src: 'boduviwaha.lk/assets/images/features/2.png',
   },
   slider3: {
-    src: '/boduviwaha/assets/images/features/3.jpg',
+    src: 'boduviwaha.lk/assets/images/features/3.jpg',
   },
   uber: {
-    src: '/boduviwaha/assets/images/clients/uber.png',
+    src: 'boduviwaha.lk/assets/images/clients/uber.png',
   },
   google: {
-    src: '/boduviwaha/assets/images/clients/google.png',
+    src: 'boduviwaha.lk/assets/images/clients/google.png',
   },
   paypal: {
-    src: '/boduviwaha/assets/images/clients/paypal.png',
+    src: 'boduviwaha.lk/assets/images/clients/paypal.png',
   },
   microsoft: {
-    src: '/boduviwaha/assets/images/clients/microsoft.png',
+    src: 'boduviwaha.lk/assets/images/clients/microsoft.png',
   },
   dribbble: {
-    src: '/boduviwaha/assets/images/clients/dribbble.png',
+    src: 'boduviwaha.lk/assets/images/clients/dribbble.png',
   },
 };
 
