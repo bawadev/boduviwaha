923e8b4251e1ee92fe7438ece61ba47139458a5f
PasinduBm <pasindumunasinghe94@gmail.com>
2024-08-03 13:27:54 +0530

    added pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    basePath: '/boduviwaha',
-};
+import withPWA from 'next-pwa';
 
+const pwaConfig = {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+}
+
+const nextConfig = {
+    
+    basePath: '',
+};
+export const pwaConfigs =  withPWA(pwaConfig);
 export default nextConfig;
--- src/pages/privacy/index.js
+++ src/pages/privacy/index.js
@@ -3,46 +3,7 @@ import { jsx, Box, Container, Heading, Text, Link } from 'theme-ui';
 const PrivacyPolicy = () => {
   return (
     <Box as="section" sx={styles.section}>
-      <Container>
-        <Heading as="h1" sx={styles.title}>Privacy Policy</Heading>
-        <Text sx={styles.date}>Effective date: January 01, 2019</Text>
-
-        <Text sx={styles.paragraph}>
-          boduviwaha.lk Matrimony Service (pvt) Ltd (&quot;us&quot;, &quot;we&quot;, or &quot;our&quot;) operates the https://boduviwaha.lk/ website and the Mindada (මින්දද) - Marriage proposals in Sri Lanka mobile application (the &quot;Service&quot;).
-        </Text>
-
-        <Text sx={styles.paragraph}>
-          This page informs you of our policies regarding the collection, use, and disclosure of personal data when you use our Service and the choices you have associated with that data.
-        </Text>
-
-        <Heading as="h2" sx={styles.sectionHeading}>Information Collection And Use</Heading>
-        <Text sx={styles.paragraph}>
-          We collect several different types of information for various purposes to provide and improve our Service to you.
-        </Text>
-
-        <Heading as="h3" sx={styles.subHeading}>Types of Data Collected</Heading>
-        <Heading as="h4" sx={styles.subSubHeading}>Personal Data</Heading>
-        <Text sx={styles.paragraph}>
-          While using our Service, we may ask you to provide us with certain personally identifiable information that can be used to contact or identify you (&quot;Personal Data&quot;). Personally identifiable information may include, but is not limited to:
-        </Text>
-        <ul>
-          <li>Phone number</li>
-          <li>National identity card details</li>
-          <li>Photo</li>
-        </ul>
-
-        <Heading as="h4" sx={styles.subSubHeading}>Usage Data</Heading>
-        <Text sx={styles.paragraph}>
-          We may also collect information that your browser sends whenever you visit our Service or when you access the Service by or through a mobile device (&quot;Usage Data&quot;).
-        </Text>
-
-        {/* Add more sections here following the same pattern */}
-
-        <Heading as="h2" sx={styles.sectionHeading}>Contact Us</Heading>
-        <Text sx={styles.paragraph}>
-          If you have any questions or suggestions about our Privacy Policy, do not hesitate to contact us at <Link href="mailto:support@boduviwaha.lk">support@boduviwaha.lk</Link>
-        </Text>
-      </Container>
+      
     </Box>
   );
 };
